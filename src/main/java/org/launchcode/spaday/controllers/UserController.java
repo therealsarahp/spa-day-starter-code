01a53f0c6aad0b1bc7faa6fdc2262ba8672b9ec2
Sarah Poulton <poultonse@gmail.com>
2021-02-26 17:16:37 -0600

    functionalityupdated to

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -20,21 +20,35 @@ public class UserController {
     }
 
     @PostMapping("add")
-    public String processAddUserForm(Model model, @ModelAttribute User user, @RequestParam String verify){
-
-        if(user.getPassword().equals(verify)) {
-
+    public String processAddUserForm(Model model, @ModelAttribute User user, @RequestParam String verify) {
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        if (user.getPassword().equals(verify)) {
             return "user/index";
         } else {
-            int error = 1;
-            model.addAttribute("error", error);
-            model.addAttribute("username", user.getUsername());
-            model.addAttribute("email", user.getEmail());
-
-            return "redirect:add";
+            model.addAttribute("error", "Passwords do not match");
+            return "user/add";
         }
+
+    }
+//        if(user.getPassword().equals(verify)) {
+//
+//            return "user/index";
+//        } else {
+//            int error = 1;
+//            model.addAttribute("error", error);
+//        }
+//
+//        if(model.containsAttribute("error")){
+//                model.addAttribute("username", "username");
+//                model.addAttribute("email", "email");
+//            }
+//            return "redirect:add";
+//        }
 
     }
 
 
-}
+
